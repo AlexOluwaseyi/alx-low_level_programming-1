9b1d99288a9da363aa158e5bb85fccfc8c2a5280
DeeXtimony <danieldamilaredaniels@gmail.com>
2023-02-16 02:20:46 -0800

    betty 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - A program that prints the size of various computer types
- * Return 0 (Success)
+ * main - Entry point
+ *
+ * Description: 'prints the size of various types on the computer it is compiled and run on'
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char a;
