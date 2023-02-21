ab7424d99324d70ce0a3ce241b9b72a0df433b88
DeeXtimony <danieldamilaredaniels@gmail.com>
2023-02-21 06:32:07 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,3 @@
-
 include "main.h"
 
 /**
@@ -8,41 +7,44 @@ include "main.h"
  *
  * Return: no return
  */
-extern void print_times_table(int n)
+void print_times_table(int n)
 {
-	inna, b, op;
+	int a = 0, rep, b;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+	while (a <= n)
 	{
-	for (a = 0; a <= n; a++)
-	{
-	_putchar(48);
-	for (b = 1; b <= n; b++)
-	{
-	op = a * b;
-	_putchar(44);
-	_putchar(32);
-	if (op <= 9)
-	{
-	_putchar(32);
-	_putchar(32);
-	_putchar(op + 48);
-	}
-	else if (op <= 99)
-	{
-	_putchar(32);
-	_putchar((op / 10) + 48);
-	_putchar((op % 10) + 48);
-	}
-	else
-	{
-	_putchar(((op / 100) % 10) + 48);
-	_putchar(((op / 10) % 10) + 48);
-	_putchar((op % 10) + 48);
-	}
-	}
-	_putchar('\n');
-	}
+		for (b = 0; b <= n; b++)
+		{
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		a++;
 	}
 }
-
